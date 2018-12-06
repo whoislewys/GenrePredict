689dfcea9babeb2d65027795ad38a13e69caa393
Luis Gomez <whoislewys@gmail.com>
2018-12-05 20:20:40 -0700

    add models dir

--- src/retrain.py
+++ src/retrain.py
@@ -18,7 +18,7 @@ X_stack = np.squeeze(np.stack((X,) * 3, -1))
 print('splitting train & test sets like moses parting the red sea')
 X_train, X_test, y_train, y_test = train_test_split(X_stack, y, test_size=0.3, random_state=42, stratify = y)
 
-MODEL_SAVE_PATH = '../models/genrepredict.h5'
+MODEL_SAVE_PATH = '../models/genrepredict-12-5-2018.h5'
 checkpointer = ModelCheckpoint(filepath=MODEL_SAVE_PATH, verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
 callbacks_list = [checkpointer]
 model = load_model(MODEL_SAVE_PATH)
